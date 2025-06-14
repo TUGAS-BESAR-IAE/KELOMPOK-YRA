9f4cc45df9c7a9d14fe2c9cc9989b8b22f448362
yohanpratamaa <yohanpratamaa@gmail.com>
2025-06-14 09:16:54 +0700

    update

--- Ternaqan/frontend-ternaqan/src/features/transactions/Transactions.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import TransactionsList from "./TransactionsList";
-import "./transactions.css";
-
-function Transactions() {
-  return <TransactionsList />;
-}
-
-export default Transactions;
